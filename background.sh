a44aa6accf48e73ff01a7fdedf8080a6dc0c9fbc
lizqwerscott <lizqwerscott@163.com>
2021-08-08 12:44:10 +0800

    randomize background

--- background.sh
+++ background.sh
@@ -5,8 +5,8 @@
 while true; do
     #feh --recursive --bg-max ~/Pictures/background/wallpaper-cityscape-design-art-world-fantasy-futuristic-wallpapers.jpg
     #feh --recursive --bg-max ~/Pictures/background/d0753e7b32.jpg
-    feh --recursive --bg-max ~/Pictures/background/Konachan.com-2243-blood.png
-    #feh --recursive --randomize --bg-max ~/Pictures/background/
+    #feh --recursive --bg-max ~/Pictures/background/Konachan.com-2243-blood.png
+    feh --recursive --randomize --bg-max ~/Pictures/background/
   sleep 30 
 done
 
